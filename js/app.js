03315d8cea83c6b93db86a2b2e2aafc236c0025a
Fernando <fernandodof@gmail.com>
2017-02-19 11:38:04 -0300

    Add function to change player, change speeds, fix bugs

--- js/app.js
+++ js/app.js
@@ -17,10 +17,14 @@ Enemy.prototype.update = function(dt) {
     this.x += this.speed * dt;
 
     //Enemies go back to the begining afeter reaching the width of the canvas
+    //And Y location is random
     if (this.x >= 505) {
         this.x = 0;
+        this.y = getRandomYLocation();
+        this.speed = getRandomSpeed();
     }
 
+    //check for colisions on every update
     collisionCheck(this);
 };
 
@@ -32,11 +36,11 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y, speed) {
+var Player = function(x, y, speed, sprite) {
     this.x = x;
     this.y = y;
     this.speed = speed;
-    this.sprite = 'images/char-boy.png';
+    this.sprite = sprite;
 };
 
 Player.prototype.update = function() {
@@ -52,6 +56,7 @@ Player.prototype.reset = function() {
     this.x = 205.5
     this.y = 383
     this.speed = 50;
+    this.sprite = playerSprite;
 };
 
 Player.prototype.handleInput = function(key) {
@@ -71,8 +76,12 @@ Player.prototype.handleInput = function(key) {
     }
 };
 
+var Gem = function(x, y, sprite) {
+
+};
+
 var collisionCheck = function(aThing) {
-    if ((player.x + 131 >= aThing.y + 90) && (player.x + 25 <= aThing.x + 88) && (player.y + 73 <= aThing.y + 135) && (player.x + 76 >= aThing.x + 11)) {
+    if ((player.y + 131 >= aThing.y + 90) && (player.x + 25 <= aThing.x + 88) && (player.y + 73 <= aThing.y + 135) && (player.x + 76 >= aThing.x + 11)) {
         console.log('Enemy collision');
         player.x = 202.5;
         player.y = 383;
@@ -84,48 +93,75 @@ var collisionCheck = function(aThing) {
 
         level++;
         increaseDifficulty(level);
-
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, 505, 171);
     }
 
     // prevent player from going out of bounds
     if (player.y > 383) {
         player.y = 383;
     }
+
     if (player.x > 402.5) {
         player.x = 402.5;
     }
+
     if (player.x < 2.5) {
         player.x = 2.5;
     }
 
 };
 
-// Create more enemiew based on level
+// Create more enemies based on level
 var increaseDifficulty = function(level) {
     // remove all previous enemies
     allEnemies.length = 0;
 
     // create all enemies
     for (var i = 0; i <= level; i++) {
-        var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+        var enemy = new Enemy(0, getRandomYLocation(), getRandomSpeed());
         allEnemies.push(enemy);
     }
+};
+
+
+//Util functions bellow
+var getRandomYLocation = function() {
+    return (Math.random() * 210) + 40;
+};
+
+var getRandomSpeed = function() {
+    return (Math.random() * 200) + 10;
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var player = new Player(205.5, 383, 50);
-var allEnemies = [];
-var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
-var level = 1;
-allEnemies.push(enemy);
+var player;
+var allEnemies;
+var enemy;
+var level;
+var playerSprite = 'images/char-boy.png';
 
 //functions bellow are called on screen buttons
-var resetGame = function() {
 
+//Reset or init game objects
+var resetGame = function() {
+    console.log(Resources.get('images/enemy-bug.png'));
+    player = new Player(200.5, 383, 50, playerSprite);
+
+    allEnemies = [];
+    enemy = new Enemy(0, getRandomYLocation(), getRandomSpeed());
+    allEnemies.push(enemy);
+
+    level = 1;
+};
+resetGame();
+
+
+//Change player and also reset game
+var changePlayer = function() {
+    playerSprite = 'images/' + $('#player-select').val();
+    console.log('images/' + $('#player-select').val());
+    resetGame();
 };
 
 // This listens for key presses and sends the keys to your
